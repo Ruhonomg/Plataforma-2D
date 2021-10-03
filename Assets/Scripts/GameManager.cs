6502302f9652e92d0afbf6d2a1fbe9ced291bded
Ruhonomg <Rubencamaron10@gmail.com>
2021-10-03 17:13:40 +0200

    EntregaV1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,24 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
     public int lives;
     public int score;
     public int starsCount;
-    public text score_Text;
+    public Text score_Text;
     public GameObject heart3;
     public GameObject heart2;
     public GameObject heart1;
     public GameObject gameOverBox;
     public GameObject victoryBox;
-    
+
+    public GameObject mainCamera;
+
     private void Start()
     {
         score_Text.text = "0";
     }
-    
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -29,12 +32,12 @@ public class GameManager : MonoBehaviour
     public void AddPoint()
     {
         score++;
-        if(score == startsCount)
+        if (score == starsCount)
         {
-            
+            victoryBox.SetActive(true);
         }
     }
-    
+
     public void ShowScore()
     {
         score_Text.text = score.ToString();
@@ -43,19 +46,23 @@ public class GameManager : MonoBehaviour
     public void LoseLive()
     {
         lives--;
-        if(lives == 2)
+        if (lives == 2)
         {
             heart3.SetActive(false);
+            StartCoroutine(mainCamera.GetComponent<CameraShake>().Shake(0.4f, 0.06f));
         }
-        if(lives == 1)
+        if (lives == 1)
         {
-            heart3.SetActive(false);
+            heart2.SetActive(false);
+            StartCoroutine(mainCamera.GetComponent<CameraShake>().Shake(0.4f, 0.06f));
         }
-        if(lives == 0)
+        if (lives == 0)
         {
-            heart3.SetActive(false);
+            heart1.SetActive(false);
+            StartCoroutine(mainCamera.GetComponent<CameraShake>().Shake(0.4f, 0.06f));
             gameOverBox.SetActive(true);
         }
+
     }
 
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioClip bombFX;
+
     private AudioSource audioSource;
 
+    // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
-    
     public void PlayBombFX()
     {
         audioSource.PlayOneShot(bombFX, 1f);
