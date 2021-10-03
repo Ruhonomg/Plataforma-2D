b729e63f240bb79b788815faca4e1cf0c39a1081
Ruhonomg <Rubencamaron10@gmail.com>
2021-10-03 23:57:23 +0200

    EtapaV8

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
